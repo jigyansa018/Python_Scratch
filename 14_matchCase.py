f1f5887baa345eb3413665779250f733a3e401cd
Jigyansa <71638450+jigyansa018@users.noreply.github.com>
2025-10-03 00:17:34 +0530

    14_matchCase.py

--- 14_matchCase.py
+++ 14_matchCase.py
@@ -17,28 +17,52 @@ match a :
 # Write a python program to print table of a number which lies between 1 and 10
 a =int(input("Enter the number lies between 1 to 10 : "))
 
-
-match a :
+match a:
     case 1:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 1")
     case 2:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 2")
     case 3:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 3")
     case 4:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 4")
     case 5:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 5")
     case 6:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 6")
     case 7:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 7")
     case 8:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 8")
     case 9:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 9")
     case 10:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
         print("case is 10")
    
     case _:
         print("No match Found")
+
+
+
+
+  
